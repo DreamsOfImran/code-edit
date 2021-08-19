cc67d6c1a69c3d7f91586a2caf3e6ee7a4460d38
Imran Basha <ibasha66@gmail.com>
2021-08-19 20:57:47 +0530

    Adding few more programming quotes

--- src/utils/quotes.js
+++ src/utils/quotes.js
@@ -7,5 +7,12 @@ export const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
   "Fix the cause, not the symptom.",
   "Simplicity is the soul of efficiency.",
-  "Make it work, make it right, make it fast."
+  "Make it work, make it right, make it fast.",
+  "Programming isn't about what you know; it's about what you can figure out.",
+  "The only way to learn a new programming language is by writing programs in it.",
+  "In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.",
+  "Testing leads to failure, and failure leads to understanding.",
+  "The best error message is the one that never shows up.",
+  "The most damaging phrase in the language is.. it's always been done this way.",
+  "Don't write better error messages, write code that doesn't need them."
 ]
